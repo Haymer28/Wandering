faef03a2aad33177d32e07f773d4e6dda6b63045
Haymer <ghaymer24gmail.com>
2022-03-08 14:50:01 -0500

    Se solucionaron los problemas de hace dos clases

--- location.py
+++ location.py
@@ -1,16 +1,16 @@
-from hashlib import new
-
-
-class location:
+class Location:
     def __init__(self):
-        self.location_de_wandering = {}
-    def add_wandering(self, wandering, locatoion):
-        self.coordenadas_de_wandering[wandering] = location
-    def move_wandering(self, wandering):
-        delta_x, delta_y = wandering.walk()
-        location_now = self.location_wandering(wandering)
-        new_location = location_now.move(delta_x,delta_y)
+        self.location_wandering = {}
+       
+    def add_wandering(self, wandering, track):
+        self.location_wandering[wandering] = track
+       
+    def move_wandering(self,wandering):
+        delt_x, delt_y = wandering.walk()
+        location_now = self.location_wandering[wandering]
+        new_location = location_now.move(delt_x, delt_y)
+       
         self.location_wandering[wandering] = new_location
-    
+   
     def get_location(self, wandering):
         return self.location_wandering[wandering]
--- wandering.py
+++ wandering.py
@@ -1,12 +1,12 @@
 import random
-
-class Wandering:
-    
-    
+ 
+class  wandering:
     def __init__(self, name):
         self.name = name
-        class CommonWandering(Wandering):
-            def __init__(self, name):
-                super().__init__(name)
-                def walk(self):
-                    return random.choice([(0,4)(0,-4)(4,0)(-4,0)])
+       
+class ComunWandering(wandering):
+    def __init__(self, name):
+        super().__init__(name)
+       
+    def walk(self):
+        return random.choice([(0,2),(0,-2),(2,0),(-2,0)])
